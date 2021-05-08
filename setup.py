5bee30214f0eb1356e439b491cd4124580385ec0
gergerov <gergerov94@gmail.com>
2021-05-08 05:12:56 +0300

    v1

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='cargo_json_fsa_fifteen',
-    version='',
+    version='1.0',
     packages=['cargo_json_fsa_fifteen'],
     url='',
     license='',
     author='gergerov',
-    author_email='',
-    description=''
+    author_email='gergerov94@gmail.com',
+    description='FSA/FSA 15 version Cargo-IMP message parser (toJson/toDict)'
 )
